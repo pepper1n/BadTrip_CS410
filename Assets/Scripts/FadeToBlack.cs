7dbfdc07a2fddcb1c3caf1a67b5d4674c3bf5407
Clara Morrison <cmorrison9528@gmail.com>
2024-06-05 02:41:54 -0700

    proc gen updates for each level

--- Assets/Scripts/FadeToBlack.cs
+++ Assets/Scripts/FadeToBlack.cs
@@ -8,18 +8,6 @@ public class FadeToBlack : MonoBehaviour
     public Image blackScreen;
     public float fadeDuration = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Fade()
     {
         float elapsedTime = 0f;
